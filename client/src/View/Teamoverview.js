99bbe9461b1795463e1e70f28884cdd0a2573bdd
casperstaehr <32480275+Lillefyren@users.noreply.github.com>
2020-12-21 01:38:46 +0100

    course Cards on frontpage

--- client/src/View/Teamoverview.js
+++ client/src/View/Teamoverview.js
@@ -2,98 +2,36 @@ import react, { useEffect, useState } from "react";
 import "../App.scss";
 import Axios from "axios";
 function Teamoverview() {
-  //usestate for course creation
-  const [titleReg, settitleReg] = useState("");
-  const [spacesReg, setspacesReg] = useState("");
-  const [startDateReg, setstartDateReg] = useState("");
-  const [endDateReg, setendDateReg] = useState("");
-  const [priceReg, setpriceReg] = useState("");
-  const [pictureReg, setpictureReg] = useState("");
-  const [adresseReg, setadresseReg] = useState("");
-  const [instructorNamesReg, setinstructorNamesReg] = useState("");
+    //usestate for course creation
+    const [data, setData] = useState([]);
 
-  Axios.defaults.withCredentials = true;
-
-  //find course table
-  Axios.get("http://localhost:3001/getcourses", {
-    title: titleReg,
-    spaces: spacesReg,
-    startDate: startDateReg,
-    endDate: endDateReg,
-    adresse: adresseReg,
-    price: priceReg,
-    picture: pictureReg,
-    instructorNames: instructorNamesReg,
-  }).then((response) => {
-    console.log(response);
-  });
-
-  return (
-    <>
-      <div className="createcourse">
-        <h1>Skab nyt kursus</h1>
-        <label>Titel</label>
-        <input
-          type="text"
-          onChange={(e) => {
-            settitleReg(e.target.value);
-          }}
-        />
-        <label>Mængden af Pladser</label>
-        <input
-          type="number"
-          onChange={(e) => {
-            setspacesReg(e.target.value);
-          }}
-        ></input>
-        <label>Start Dato</label>
-        <input
-          type="date"
-          onChange={(e) => {
-            setstartDateReg(e.target.value);
-          }}
-        ></input>
-        <label>Slut Dato</label>
-        <input
-          type="date"
-          onChange={(e) => {
-            setendDateReg(e.target.value);
-          }}
-        ></input>
-
-        <label>Adresse</label>
-        <input
-          type="text"
-          onChange={(e) => {
-            setadresseReg(e.target.value);
-          }}
-        ></input>
-
-        <label>Pris</label>
-        <input
-          type="text"
-          onChange={(e) => {
-            setpriceReg(e.target.value);
-          }}
-        />
-        <label>Billede</label>
-        <input
-          type="file"
-          onChange={(e) => {
-            setpictureReg(e.target.value);
-          }}
-        />
-        <label>Instruktør</label>
-        <input
-          type="text"
-          onChange={(e) => {
-            setinstructorNamesReg(e.target.value);
-          }}
-        />
-        <button onClick={Teamoverview}>Opret</button>
-      </div>
-    </>
-  );
+    Axios.defaults.withCredentials = true;
+  
+    //find course table
+    useEffect(()=>{
+      Axios.get("http://localhost:3001/getcourses").then((response) => {
+        setData(response.data);
+      console.log(response);
+      })
+    }, [])
+  console.log("okok");
+    return (
+      <>
+          {data.map((course)=>{return(
+            <div className="Card">
+          <div className="Card__body">
+            <h2 className="Card__title">{course.CourseTitle}</h2>
+            <p className="Card__instructor">{course.CourseInstructorNames}</p>
+            <p className="Card__instructor">{course.CourseDescription}</p>
+            <p className="Card__price">{course.CoursePrice}</p>
+            <p className="Card__spaces">{course.CourseSpaces}</p>
+            <p className="Card__start-date">{course.CourseStartDate}</p>
+            <p className="Card__end-date">{course.CourseEndDate}</p>
+          </div>
+          </div>
+          )})}
+      </>
+    );
 }
 
 export default Teamoverview;
